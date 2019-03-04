7f349d9a11909e88cd6877f23f88249ea6a40e7f
Dariush Aligholizadeh <daligholizadeh@yahoo.com>
2019-03-03 20:02:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ setInterval(function(){
 	document.body.style.backgroundImage = document.getElementById('calendar').style.backgroundImage = `linear-gradient(to right, rgb(${r}, ${g}, ${b}), rgb(${r2}, ${g2}, ${b2})`;
 }, 50);
 
-const activitiestext = new Array(100); //maybe increase array size because 100 events is a bit low
-//activitiestext.fill("");
+const activitiestext = new Array(100);
 let indice = 0;
 
 let months = {
@@ -51,13 +50,6 @@ for(let z = 0; z < 35; z++){
 	} else document.getElementsByTagName('TD')[z].innerText = "";
 }
 
-
-console.log(`\n\n\n\n\n${tdelements[10]}\n\n\n\n\n\n`)
-
-
-
-//TRY AND FIX THIS THIS DOESNT WORK
-
 document.getElementById('close').addEventListener('click', function(){
 	document.getElementById('prompt').style.display = "none";
 	document.getElementById('input').value = "";
@@ -65,41 +57,6 @@ document.getElementById('close').addEventListener('click', function(){
 
 let clicked_add = false;
 
-
-/*tdelements.forEach(function(elem, index){
-	console.log(index);
-	//if(index >= months.february.startdate && index <= months.february.enddate){	
-	elem.addEventListener('click', function(){
-			console.log(event.target + "\n" + index)
-		document.getElementById('prompt').style.display = "block";
-		document.getElementById('textlabel').innerText = `Enter your activities for February ${(index - months.february.startdate) + 1}`;
-		//since im adding a click listener its going off as soon as the first is being clicked so its not really listenening which is why im adding something to stop that
-		//document.getElementById('input').value = index;
-		document.getElementById('add').addEventListener('click', function(){
-			console.log(event.target)
-			if(document.getElementById('input').value != ""){	
-				vent = (index - months.february.startdate + 1) + " " + document.getElementById('input').value;
-				activitiestext[indice] = index + ">>" + vent; //use .split('>> to split up the markers with the events')
-				indice++;
-				document.getElementById('input').value = "";
-			}
-			console.log(activitiestext);
-			console.log(index - months.february.startdate + 1);
-			//document.getElementById('status').innerText = "added";
-			
-		});
-		//use the indices and start making a parsing function seperating indices into events
-		document.getElementById('check').addEventListener('click', function(){
-			document.getElementById('prompt').style.display = 'none';
-			document.getElementById('eventview').innerText = activitiestext[index];
-		});
-		//close event listener
-		
-	});
-	//}
-})*/
-//check for the same event using split and then use change the last and first element with all of them and then start popping off the end all indices of that
-//or you can sort it and put all the fake ones at the end and output a length equal to those that were inputted
 function thismightwork(){
 	for(let z = 1; z < 99; z++){
 		if(activitiestext[z].split(">>")[1] == activitiestext[z+1].split(">>")[1] && (activitiestext[z] != "")){
@@ -109,15 +66,6 @@ function thismightwork(){
 		}
 	}
 }
-
-/*
-
-
-
-WHATEVER IM DOING IS EXTREMELY STUPID JUST MAKE ONE SINGLE EVENTLISTENER FOR THE ADDING FUNCTION AND THEN HAVE THE STUFF IN THERE
-
-
-*/
 
 function outputentries(check){
 	let mutablearray = new Array(activitiestext.length);
@@ -159,7 +107,7 @@ function outputentries(check){
 
 	console.log(`${count}\n${mutablearray}\n${returnarray}`)
 
-	return string; //some array with formatting
+	return string;
 }
 
 document.getElementById('add').addEventListener('click', function(){
@@ -168,13 +116,7 @@ document.getElementById('add').addEventListener('click', function(){
 		activitiestext[indice] = (tdelements[football].innerText) + ">>" + document.getElementById('input').value; //use .split('>> to split up the markers with the events')
 		indice++;
 		document.getElementById('input').value = "";
-	}
-
-
-	console.log(activitiestext);
-	//console.log(x - months.february.startdate + 1);
-	//document.getElementById('status').innerText = "added";
-	
+	}	
 });
 
 document.getElementById('check').addEventListener('click', function(){
@@ -188,7 +130,7 @@ document.getElementById('check').addEventListener('click', function(){
 let football = 0;
 
 function checktoseeifworks(){
-	//should probably fix this next part later, its gonna get messy
+	
 	for(let z = 1; z < 100; z+=(z*2)){
 		if(activitiestext[z] != ""){
 			if(z == 1){	
@@ -201,54 +143,16 @@ function checktoseeifworks(){
 					activitiestext[z + a] = "";
 				}
 			}
-			//activitiestext.splice(activitiestext[z], z);
-			//break;
 		}
 	}
 }
 
 for(let x = months.february.startdate; x <= months.february.enddate; x++){
 	console.log(x);
-	//if(index >= months.february.startdate && index <= months.february.enddate){	
 	tdelements[x].addEventListener('click', function(){
 		console.log(event.target + "\n" + x)
 		football = x;
 		document.getElementById('prompt').style.display = "block";
-		document.getElementById('textlabel').innerText = `Enter your activities for February ${(x - months.february.startdate) + 1}`;
-		//since im adding a click listener its going off as soon as the first is being clicked so its not really listenening which is why im adding something to stop that
-		//document.getElementById('input').value = index;		
-		//document.getElementById('input').value = "";
-		//use the indices and start making a parsing function seperating indices into events
-		
-		//close event listener
-		
+		document.getElementById('textlabel').innerText = `Enter your activities for February ${(x - months.february.startdate) + 1}`;		
 	});
 }
-
-// for(let x = months.february.startdate; x <= months.february.enddate; x++){
-// 	document.getElementsByTagName('TD')[x].addEventListener('click', function(){
-// 		//alert('test' + (x+1));
-// 		//document.getElementById('status').innerText = activitiestext[x];
-// 		document.getElementById('add').addEventListener('click', function(){
-// 			if(document.getElementById('input').value != "" || document.getElementById('input').value != " "){
-// 				let event = (x - months.february.startdate) + document.getElementById('input').value + "\n";
-// 				activitiestext[indice] = event;
-// 				indice++;
-// 			}
-// 			document.getElementById('input').value = "";
-// 			console.log(activitiestext + "\n" + indice);
-// 			//document.getElementById('status').innerText = "added";
-// 			document.getElementById('check').addEventListener('click', function(){
-// 				document.getElementById('prompt').style.display = 'none';
-// 				document.getElementById('eventview').innerText += activitiestext[x];
-// 			});
-// 		});
-// 		document.getElementById('close').addEventListener('click', function(){
-// 			document.getElementById('prompt').style.display = "none";
-// 			document.getElementById('status').innerText = "";
-// 			document.getElementById('input').value = "";
-// 		});
-// 		document.getElementById('prompt').style.display = "block";
-// 		document.getElementById('textlabel').innerText = `Enter your activities for February ${(x - months.february.startdate) + 1}`;
-// 	});
-// }
